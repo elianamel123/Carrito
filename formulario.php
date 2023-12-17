0a719e60d4d89bab8bbf17125c839e44dd02bf5f
elianamel123 <131275318+elianamel123@users.noreply.github.com>
2023-12-16 21:49:29 -0500

    enviar

--- formulario.php
+++ formulario.php
@@ -8,7 +8,7 @@ include ("include/conexion.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carrito de Compras</title>
+    <title>Carrito de Compras CARRITO</title>
     <link rel="stylesheet" href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" type="text/css" >
     <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
