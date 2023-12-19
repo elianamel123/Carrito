953db53a9df5828cca63326f6cd2f18ab73de1fe
elianamel123 <131275318+elianamel123@users.noreply.github.com>
2023-12-18 23:18:34 -0500

    enviar git

---
+++ include/conexion.php
@@ -0,0 +1,16 @@
+<?php
+// variables de la base de datos
+$servidor = "localhost";
+$bd = "carrito_venta";
+$usuario = "root";
+$password = "";
+
+$conexion = mysqli_connect($servidor, $usuario, $password, $bd);
+
+// verificar la conexion
+if ( !$conexion == true) {
+    # code...
+    echo "conexion fallida";
+}
+
+?>
