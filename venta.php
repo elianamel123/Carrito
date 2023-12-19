17e03aad624c054d8f3993b6433086e02a7925b6
elianamel123 <131275318+elianamel123@users.noreply.github.com>
2023-12-18 23:17:16 -0500

    enviar ventas

--- venta.php
+++ venta.php
@@ -1,20 +1,16 @@
 <?php
-include("include/conexion.php");
+include('include/conexion.php');
 session_start();
-$_SESSION ['productos'] = array();
-
-
+$_SESSION['productos'] = array();
 ?>
 
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ventas</title>
+    <title>Document</title>
     <link rel="stylesheet" href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" type="text/css">
     <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
@@ -28,52 +24,46 @@ $_SESSION ['productos'] = array();
     <!-- Script obtenido desde CDN jquery -->
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
-    <style>
-        .cantidad{
-            width: 3em;
 
+    <style>
+        .cantidad {
+            width: 3em;
         }
     </style>
-
 </head>
 
+
 <body>
-
-
     <?php
-    //lenguaje en php
+    // lenguaje en php
     include("include/menu.php");
     ?>
-
-    <!--Inicio de Contenido-->
+    <!-- INICIO DE CONTENIDO -->
     <div class="main-content">
         <div class="page-content">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
-
-
-                        <h4 class="">Registrar Nueva Venta</h4>
+                        <h4>Registrar Nueva Venta</h4>
                         <div class="card">
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-lg-12 col-md-12 col-sm-12">
-                                        <form action="">
+                                        <form role="form" action="operaciones/registrar_venta.php" method="POST">
                                             <div class="form-group row">
-                                                <label class="col-lg-2 col-md-2 col-sm-12"> DNI: </label>
-                                                <input type="number" name="dni" class="form-control col-lg-4 col-md-4 col-sm-12" required placeholder="Buscar">
-                                                <button class="btn btn-danger form-control col-lg-2 col-md-1 col-sm-4 ">Buscar</button>
+                                                <label class="col-lg-2 col-md-2 col-sm-6">DNI: </label>
+                                                <input type="number" name="dni" id="dni_cliente" class="form-control col-lg-4 col-md-4 col-sm-12" required placeholder="dni cliente">
+                                                <label class="col-lg-1 col-md-1 col-sm-1"></label>
+                                                <button type="button" class="btn btn-danger col-lg-1 col-md-1 col-sm-2" onclick="buscar_cliente();">Buscar</button>
                                             </div>
-
                                             <div class="form-group row">
-                                                <label class="col-lg-2 col-md-2 col-sm-12"> Apellidos y Nombres: </label>
+                                                <label class="col-lg-2 col-md-2 col-sm-6">Apellidos y Nombres: </label>
                                                 <div id="cliente_datos" class=" col-lg-6 col-md-6 col-sm-12">
                                                     <input type="text" class="form-control col-lg-6 col-md-6 col-sm-12" readonly>
                                                 </div>
-
                                             </div>
                                             <div class="form-group row">
-                                                <label class="col-lg-2 col-md-2 col-sm-12"> Usuario: </label>
+                                                <label class="col-lg-2 col-md-2 col-sm-6">Usuario: </label>
                                                 <select name="usuario" id="usuario" class="form-control col-lg-6 col-md-6 col-sm-12">
                                                     <option value=""></option>
                                                     <?php
@@ -85,90 +75,69 @@ $_SESSION ['productos'] = array();
 
                                                     ?>
                                                 </select>
-
-
                                             </div>
-
                                             <div class="form-group row">
-                                                <label class="col-lg-2 col-md-2 col-sm-12"> Producto: </label>
-                                                <input type="text" name="producto"  id="producto" class="form-control col-lg-4 col-md-4 col-sm-12" required placeholder="Buscar">
-                                                <button type="button" class="btn btn-danger form-control col-lg-2 col-md-1 col-sm-4 " onclick="agregar_producto();" >Agregar</button>
+                                                <label class="col-lg-2 col-md-2 col-sm-6">producto: </label>
+                                                <input type="number" name="producto" id="producto" class="form-control col-lg-4 col-md-4 col-sm-12" placeholder="código producto">
+                                                <label class="col-lg-1 col-md-1 col-sm-1"></label>
+                                                <button type="button" class="btn btn-danger col-lg-1 col-md-1 col-sm-2" onclick="agregar_producto();">Agregar</button>
                                             </div>
-
                                             <div class="form-group row">
-                                                <label class="col-lg-2 col-md-2 col-sm-12"> Fecha y Hora: </label>
-                                                <label for="" class="form-control col-lg-4 col-md-4 col-sm-12">
-                                                    <?php 
+                                                <label class="col-lg-2 col-md-2 col-sm-6">Fecha y Hora: </label>
+                                                <label for="" class="form-control col-lg-2 col-md-2 col-sm-6">
+                                                    <?php
                                                     date_default_timezone_set("America/Lima");
-                                                    echo date("d-m-Y h:i:s") ?>
+                                                    echo date("d-m-Y h:i:s"); ?>
                                                 </label>
-                                                
-
-
                                             </div>
                                             <div class="form-group row">
                                                 <button type="submit" class="btn btn-danger">Registrar Venta</button>
                                             </div>
-
-
-
                                         </form>
-
-
                                     </div>
                                     <div class="col-lg-12 col-md-12 col-sm-12">
                                         <table class="table table-bordered mb-0">
                                             <thead>
                                                 <tr>
-                                                    <th colspan="6" class="text-center">Producto</th>
+                                                    <th colspan="6" class="text-center">PRODUCTOS</th>
                                                 </tr>
                                                 <tr>
-                                                    <th width= "5%">Num:</th>
-                                                    <th width= "60%">Descripcion</th>
-                                                    <th width= "10%">Cant.</th>
-                                                    <th width= "10%">P. Unit.</th>
-                                                    <th width= "10%">Importe</th>
-                                                    <th width= "5%"></th>
+                                                    <th width="5%">Nro</th>
+                                                    <th width="60%">Descripción</th>
+                                                    <th width="10%">Cant.</th>
+                                                    <th width="10%">P.unit.</th>
+                                                    <th width="10%">Importe</th>
+                                                    <th width="5%"></th>
                                                 </tr>
                                             </thead>
                                             <tbody id="contenido_tabla">
                                                 <?php
-                                                $array_productos = $_SESSION ["productos"];
+                                                $array_productos = $_SESSION['productos'];
                                                 foreach ($array_productos as $key => $value) {
-                                                    // Key => id Value => Cantidad
-                                                    $consulta = "SELECT * FROM producto WHERE id=$key";
-                                                    $ejecutar = mysqli_query($conexion,$consulta);
+                                                    // key => id      value => cantidad
+                                                    $consulta = "SELECT * FROM producto WHERE id='$key'";
+                                                    $ejecutar = mysqli_query($conexion, $consulta);
                                                     $producto = mysqli_fetch_array($ejecutar);
-                                                
-                                                
-                                                
+
                                                 ?>
-                                                <tr>
-                                                    <td>1</td>
-                                                    <td>Nombre Producto</td>
-                                                    <td> <input type="number" value="1" class="cantidad" onchange="actualizar_cantidad(id);" > </td>
-                                                    <td>S/.50.00</td>
-                                                    <td>S/.100.00</td>
-                                                    <td><button class="btn btn-info" onclick="eliminar_producto(id);">x</button></td>
-                                                </tr>
+                                                    <tr>
+                                                        <td>1</td>
+                                                        <td>nombre producto</td>
+                                                        <td><input type="number" value="2" class="cantidad" onchange="actualizar_cantidad(id);"></td>
+                                                        <td>S/. 50.00</td>
+                                                        <td>S/. 100.00</td>
+                                                        <td><button class="btn btn-danger" onclick="eliminar_producto(id);">X</button></td>
+                                                    </tr>
                                                 <?php } ?>
                                                 <tr>
                                                     <td colspan="4" class="text-center">TOTAL</td>
-                                                    <td>S/.100.00</td>
+                                                    <td>S/. 100.00</td>
                                                 </tr>
                                             </tbody>
-
                                         </table>
-
-
-
-
                                     </div>
                                 </div>
-
-
                             </div>
-
                         </div>
                     </div>
                 </div>
@@ -176,9 +145,7 @@ $_SESSION ['productos'] = array();
         </div>
     </div>
 
-    <!--Fin de Contenido-->
-
-
+    <!-- FIN DE CONTENIDO -->
 
     <!-- jQuery  -->
     <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
@@ -205,31 +172,31 @@ $_SESSION ['productos'] = array();
 
     <!-- Datatables init -->
     <script src="plantilla/Admin/vertical/assets/pages/datatables-demo.js"></script>
-
     <!-- App js -->
     <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
 
     <script>
-        function agregar_producto(){
+        function agregar_producto() {
             var codigo = $('#producto').val();
             $.ajax({
                 type: "POST",
-                url: "Operaciones/agregar_producto.php",
-                data:{cod:codigo},
-                success: function(r){
+                url: "operaciones/agregar_producto.php",
+                data: {
+                    cod: codigo
+                },
+                success: function(r) {
                     $('#contenido_tabla').html(r);
-
                 }
             })
             document.getElementById('producto').value = '';
 
-
         };
+
         function actualizar_cantidad(id) {
             var cantidad = $('#cantidad_' + id).val();
             $.ajax({
                 type: "POST",
-                url: "Operaciones/actualizar_cantidad.php",
+                url: "operaciones/actualizar_cantidad.php",
                 data: {
                     id_p: id,
                     cant: cantidad
@@ -240,11 +207,17 @@ $_SESSION ['productos'] = array();
             })
         };
 
-        
-        function eliminar_producto(id){
-            
-            
-            
+        function eliminar_producto(id) {
+            $.ajax({
+                type: "POST",
+                url: "Operaciones/eliminar_producto_tabla.php",
+                data: {
+                    id_p: id
+                },
+                success: function(r) {
+                    $('#contenido_tabla').html(r);
+                }
+            })
 
 
         };
@@ -253,7 +226,7 @@ $_SESSION ['productos'] = array();
             var dni = $('#dni_cliente').val();
             $.ajax({
                 type: "POST",
-                url: "Operaciones/buscar_cliente.php",
+                url: "operaciones/buscar_cliente.php",
                 data: {
                     dni_c: dni
                 },
@@ -263,7 +236,6 @@ $_SESSION ['productos'] = array();
             })
         };
     </script>
-
 </body>
 
 </html>
